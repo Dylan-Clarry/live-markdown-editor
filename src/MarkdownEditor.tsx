0bfa434fd4fb08a9ff25694bf88ebf4e7b8947ba
Dylan <dylan.imail@gmail.com>
2023-03-15 21:05:51 -0400

    basic editor working

--- src/MarkdownEditor.tsx
+++ src/MarkdownEditor.tsx
@@ -1,15 +1,21 @@
-import { useCallback } from 'react';
+import { useCallback } from "react";
 import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
 
 export default function MarkdownEditor() {
-    const onChange = useCallback((value, viewUpdate) => {
-
-    };
+    const onChange = useCallback((value: any, viewUpdate: any) => {
+        console.log("value", value);
+    }, []);
 
     return (
         <div>
             <h1>Editor</h1>
-            <CodeMirror value="console.log('hello world!');" height="200px" extensions={[javascript({ jsx: true })]} onChange={onChange} />
+            <CodeMirror
+                value="console.log('hello world!');"
+                height="200px"
+                extensions={[javascript({ jsx: true })]}
+                onChange={onChange}
+            />
         </div>
     );
 }
